2ff09a7a8cdf6e10c7f177c66b00a409efc50d50
Long Mai <mai.t.long88@gmail.com>
2021-03-07 17:45:40 -0800

    update run script to select GPU

--- MTL_scratch_script.sh
+++ MTL_scratch_script.sh
@@ -1,2 +1,4 @@
 
-python test.py --video examples/dogs_jump_orig.mp4 --mask examples/dogs_jump  --ckpt checkpoints/sttn.pth 
+
+CUDA_VISIBLE_DEVICES=3 python test.py --video examples/schoolgirls_orig.mp4 --mask examples/schoolgirls  --ckpt checkpoints/sttn.pth 
+
